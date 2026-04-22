19cfe37b6f976a4d6b4e1f20a6042715ea437695
Maëlick Claes <maelick.claes@ductus.se>
2026-04-22 15:48:23 +0200

    add logger to thermostat service

--- thermostat-service/src/main/java/se/ductus/thermostat/service/ThermostatService.java
+++ thermostat-service/src/main/java/se/ductus/thermostat/service/ThermostatService.java
@@ -8,6 +8,8 @@ import jakarta.inject.Singleton;
 import jakarta.ws.rs.NotFoundException;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.ductus.temperaturesensor.model.Heating;
 import se.ductus.temperaturesensor.model.Temperature;
 import se.ductus.temperaturesensor.service.TemperatureSensorService;
@@ -20,6 +22,7 @@ import io.quarkus.scheduler.Scheduled;
 
 @Singleton
 public class ThermostatService {
+    private static final Logger log = LoggerFactory.getLogger(ThermostatService.class);
 
     @Inject
     TemperatureSetpointRepository temperatureSetpointRepository;
@@ -44,29 +47,42 @@ public class ThermostatService {
     }
 
     @Scheduled(every = "1s")
-    void controlTemperature() {
+    synchronized void controlTemperature() {
+        log.info("Getting setpoints");
+
         List<TemperatureSetpoint> temperatureSetpoints = new ArrayList<>();
         for (String temperatureSensor : temperatureSensors) {
             try {
-                TemperatureSetpoint temperatureSetpoint= this.getSetpoint(temperatureSensor);
+                TemperatureSetpoint temperatureSetpoint = this.getSetpoint(temperatureSensor);
                 temperatureSetpoints.add(temperatureSetpoint);
             }
             catch (NotFoundException ignored) {
-
+                log.warn("setpoint not found for sensor {}", temperatureSensor);
             }
         }
+
+        log.info("Checking sensor temperatures");
         for (TemperatureSetpoint temperatureSetpoint : temperatureSetpoints) {
             String url = String.format("http://%s:8080", temperatureSetpoint.temperatureSensorId);
             Temperature temperature = temperatureSensorService.getTemperature(url);
             if (temperature.celsius < temperatureSetpoint.celsius) {
+                log.info("Setting sensor {} heating=true (temperature={}, setpoint={})",
+                        temperatureSetpoint.temperatureSensorId,
+                        temperature.celsius,
+                        temperatureSetpoint.celsius
+                );
                 temperatureSensorService.setHeating(url, new Heating(true));
             }
             if (temperatureSetpoint.celsius > temperature.celsius) {
+                log.info("Setting sensor {} heating=false (temperature={}, setpoint={})",
+                        temperatureSetpoint.temperatureSensorId,
+                        temperature.celsius,
+                        temperatureSetpoint.celsius
+                );
                 temperatureSensorService.setHeating(url, new Heating(false));
             }
 
         }
-
     }
 
     public void updateSetpoint(TemperatureSetpoint temperatureSetpoint) {
