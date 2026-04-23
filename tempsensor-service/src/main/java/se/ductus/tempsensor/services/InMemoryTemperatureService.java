a8505a13441f4e3df2871431d9a019ac2aa27d39
Maëlick Claes <maelick.claes@ductus.se>
2026-04-23 14:43:43 +0200

    configurable sensor update interval

--- tempsensor-service/src/main/java/se/ductus/tempsensor/services/InMemoryTemperatureService.java
+++ tempsensor-service/src/main/java/se/ductus/tempsensor/services/InMemoryTemperatureService.java
@@ -69,7 +69,7 @@ public class InMemoryTemperatureService implements TemperatureService {
         }
     }
 
-    @Scheduled(every = "1s", concurrentExecution = Scheduled.ConcurrentExecution.SKIP)
+    @Scheduled(every = "${se.ductus.tempsensor.temperature-update-interval:1s}", concurrentExecution = Scheduled.ConcurrentExecution.SKIP)
     void updateTemperature() {
         var lock = this.lock.writeLock();
         lock.lock();
