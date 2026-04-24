732a6e456904781bc41081ae2e2af9df906ffba2
Maëlick Claes <maelick.claes@ductus.se>
2026-04-24 10:03:18 +0200

    fix routing key

--- tempsensor-service/src/main/java/se/ductus/tempsensor/services/AMQPTemperatureBroker.java
+++ tempsensor-service/src/main/java/se/ductus/tempsensor/services/AMQPTemperatureBroker.java
@@ -1,6 +1,8 @@
 package se.ductus.tempsensor.services;
 
 import io.smallrye.reactive.messaging.rabbitmq.OutgoingRabbitMQMetadata;
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Message;
@@ -11,10 +13,14 @@ public class AMQPTemperatureBroker implements TemperatureBroker {
     @Channel("temperature-updates")
     Emitter<TemperatureSensorStateEvent> eventEmitter;
 
+    @ConfigProperty(name = "mp.messaging.outgoing.temperature-updates.default-routing-key")
+    String rootRoutingKey;
+
     @Override
     public void send(TemperatureSensorStateEvent event) {
+        var routingKey = rootRoutingKey + "." + event.sensorId();
         var metadata = new OutgoingRabbitMQMetadata.Builder()
-                .withRoutingKey("routing-key-TODO");
+                .withRoutingKey(routingKey);
         var msg = Message.of(event, Metadata.of(metadata));
         this.eventEmitter.send(msg);
     }
